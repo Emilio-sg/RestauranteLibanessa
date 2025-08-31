1e72ac69c8a7b56a4950fbf29abfaa26ad30458d
Emilio Subervi <emiliosubervi@gmail.com>
2025-08-31 17:01:01 -0400

    fix navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,11 @@
 
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import  Link  from 'react-router-dom';
 import '../styles/libanessa.css';
 
 
 const Navbar = () => {
-  /*const navigate = useNavigate();*/
+  /*const navigate = useNavigate;*/
   const user = JSON.parse(localStorage.getItem('user'));
 
   const handleLogout = () => {
